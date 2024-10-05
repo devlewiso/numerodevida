85f9a8d0ec6da457d0c9616904b65e41956338d1
Iran Trinidad <devlewiso@gmail.com>
2024-10-05 01:32:28 -0600

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Script from 'next/script'; // Importamos Script de Next.js
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,7 +10,7 @@ export const metadata = {
   openGraph: {
     title: 'Calculadora de Número de Vida',
     description: 'Calcula tu número de vida basado en tu fecha de nacimiento.',
-    url: 'https://tusitio.com',
+    url: 'https://numerodevida.netlify.app/',
     siteName: 'Calculadora de Número de Vida',
   },
   twitter: {
@@ -23,20 +24,34 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PP5M0L664Z"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PP5M0L664Z');
+          `}
+        </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "nw7ch090jo");
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         {children}
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "nw7ch090jo");
-            `,
-          }}
-        />
       </body>
     </html>
   );
